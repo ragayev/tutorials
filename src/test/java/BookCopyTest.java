7cd9835ed9be679f15c76fa70233ecc2d80cf663
Rauf Aghayev <aghayevr@amazon.com>
2022-04-23 22:27:34 +0200

    Add more tests

--- src/test/java/BookCopyTest.java
+++ src/test/java/BookCopyTest.java
@@ -1,6 +1,10 @@
-import jdk.jfr.StackTrace;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class BookCopyTest {
 
@@ -14,12 +18,29 @@ class BookCopyTest {
     }
 
     @Test
+    public void testShallowCopyFail() {
+        Author author = new Author("Rauf");
+        Book book = new Book(250, author);
+        Book clonedBook = book.shallowCopy();
+        clonedBook.getAuthor().setName("Orxan");
+        Assertions.assertNotEquals("Rauf", book.getAuthor().getName());
+    }
+
+    @Test
     public void testDeepCopy() {
         Author author = new Author("Rauf");
         Book book = new Book(250, author);
         Book clonedBook = book.deepCopy();
         clonedBook.getAuthor().setName("Orxan");
         Assertions.assertEquals("Rauf", book.getAuthor().getName());
+    }
+
+    @Test
+    public void testCollectionsImmutability() {
+        final List<Integer> finalList = new ArrayList<>();
+        final List<Integer> unmodifiableList = Collections.unmodifiableList(Arrays.asList(1, 2, 3));
+        Assertions.assertDoesNotThrow(() -> finalList.add(4));
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> unmodifiableList.add(4));
     }
 
 }
